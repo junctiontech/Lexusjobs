81b6357c897ad63ad1465abaefd7f835702c537f
sarva <sarva@LAPTOP-Q31P2N9C>
2016-09-10 14:48:00 +0530

    qwerty

--- js/common_function.js
+++ js/common_function.js
@@ -54,7 +54,7 @@
 function ValueEdit(id)
    { alert(id);
      $.ajax({ 			  
-	          url:"Master/masterUpdate",
+	          url:"Master/masterValueUpdate",
               type:"post",
 	          data:{value:id},
 		  })
